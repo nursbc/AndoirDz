846d3ae575c2acc47bd1a5de1be27080945980b3
lord_bc <wolf.1992@list.ru>
2020-07-11 16:40:30 +0300

    Dz4

---
+++ app/src/main/java/com/example/andoirdz/Dz4Activity.kt
@@ -0,0 +1,47 @@
+package com.example.andoirdz
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.dz4_activity.*
+
+class Dz4Activity : AppCompatActivity(), View.OnClickListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.dz4_activity)
+
+        initializeListeners()
+    }
+
+    fun initializeListeners()
+    {
+        dz4_activity_main_button_send_message_whatsapp.setOnClickListener(this)
+        dz4_activity_main_button_registration.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id)
+        {
+            R.id.dz4_activity_main_button_send_message_whatsapp -> {
+                sendMessageWhatsAppFriend("+77072008993", "???")
+            }
+            R.id.dz4_activity_main_button_registration -> {
+                val intent = Intent(this, Dz4RegistratoinActivity::class.java)
+                startActivity(intent)
+            }
+        }
+
+    }
+
+    fun sendMessageWhatsAppFriend(number: String, message:String){
+        val friendnumber = number
+        val sendIntent = Intent(Intent.ACTION_SEND)
+        sendIntent.type = "text/plain"
+        sendIntent.putExtra(Intent.EXTRA_TEXT, message)
+        sendIntent.putExtra("jid", "$friendnumber@s.whatsapp.net")
+        sendIntent.setPackage("com.whatsapp")
+        startActivity(sendIntent)
+    }
+}
---
+++ app/src/main/java/com/example/andoirdz/Dz4RegistratoinActivity.kt
@@ -0,0 +1,41 @@
+package com.example.andoirdz
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.dz4_activity_regestration.*
+
+class Dz4RegistratoinActivity : AppCompatActivity(), View.OnClickListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.dz4_activity_regestration)
+
+        initializeListener()
+    }
+
+    fun initializeListener() {
+        dz4_activity_registration_button_send.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.dz4_activity_registration_button_send -> {
+                if (activity_registration_edit_text_login!!.text.isNotEmpty() && activity_registration_edit_text_password!!.text.isNotEmpty()) {
+                    sendMessageWhatsApp("User:${activity_registration_edit_text_login?.text} password:${activity_registration_edit_text_password?.text}")
+                }
+            }
+        }
+    }
+
+    fun sendMessageWhatsApp(message:String){
+        val sendIntent = Intent()
+        sendIntent.action = Intent.ACTION_SEND
+        sendIntent.putExtra(Intent.EXTRA_TEXT, message)
+        sendIntent.type = "text/plain"
+        sendIntent.setPackage("com.whatsapp")
+        startActivity(sendIntent)
+    }
+}
